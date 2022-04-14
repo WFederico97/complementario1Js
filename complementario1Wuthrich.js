801c1b053dfde92cc3733b79cc5a5175aa8f05ba
Federico <wuthrich09@gmail.com>
2022-04-14 19:09:59 -0300

    Js, html5

---
+++ complementario1Wuthrich.js
@@ -0,0 +1,8 @@
+let pregunta = prompt("¿Eres Fan de Hasbulla Y/N?")
+let i 
+for (let i  = 1; i<=3; i ++) {
+    alert("seguro?");
+    alert("deveritas?");
+    alert("Bueno, te creo... bienvenido a mi pagina");
+    break;
+}
